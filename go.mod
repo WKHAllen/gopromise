9b221a6a72849990c5323b25bdf801a2429ccdd4
WKHAllen <wkhallen@gmail.com>
2020-07-03 20:28:48 -0400

    Implement Promise, All, and Race

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gopromise
+
+go 1.13
---
+++ gopromise.go
@@ -0,0 +1,71 @@
+package gopromise
+
+// Promise represents a pending task
+func Promise(callback func() interface{}) <-chan interface{} {
+	p := make(chan interface{})
+
+	go func() {
+		defer close(p)
+		p <- callback()
+	}()
+
+	return p
+}
+
+// All will run multiple pending tasks
+func All(callbacks ...func() interface{}) <-chan interface{} {
+	p := make(chan interface{})
+
+	go func() {
+		defer close(p)
+
+		remaining := len(callbacks)
+		retVals := make([]interface{}, len(callbacks))
+
+		for idx, callback := range callbacks {
+			go func(index int, callbackFunc func() interface{}) {
+				retVals[index] = callbackFunc()
+				remaining--
+			}(idx, callback)
+		}
+
+		for remaining > 0 {}
+
+		for _, retVal := range retVals {
+			p <- retVal
+		}
+	}()
+
+	return p
+}
+
+// Call callbacks using recursive select statements
+func raceRecursive(callbacks []func() interface{}) <-chan interface{} {
+	switch len(callbacks) {
+		case 0: return nil
+		case 1: return Promise(callbacks[0])
+		default:
+			return Promise(func() interface{} {
+				var result interface{}
+	
+				select {
+					case result = <-Promise(callbacks[0]):
+					case result = <-raceRecursive(callbacks[1:]):
+				}
+	
+				return result
+			})
+	}
+}
+
+// Race will run multiple tasks and return the first one completed
+func Race(callbacks ...func() interface{}) <-chan interface{} {
+	p := make(chan interface{})
+
+	go func() {
+		defer close(p)
+		p <- (<-raceRecursive(callbacks))
+	}()
+
+	return p
+}
